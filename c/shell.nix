4b96c40fdd52a97feb8d67a89cff4273f3fd8455
Michael Greenly <mgreenly@gmail.com>
2025-05-04 11:35:40 -0500

    move back to nix shell for building

---
+++ c/shell.nix
@@ -0,0 +1,19 @@
+let
+  tarball = fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/24.11.tar.gz";
+  pkgs = import tarball { config = {}; overlays = []; };
+in
+
+pkgs.mkShell {
+
+  packages = with pkgs; [
+  ];
+
+  nativeBuildInputs = with pkgs; [
+    pkg-config
+  ];
+
+  shellHook = with pkgs; ''
+    export LOCALE_ARCHIVE="${glibcLocales}/lib/locale/locale-archive";
+    export PS1='\[\e[93;1;2m\]nix-shell@\W\[\e[0m\]\\$ '
+  '';
+}
